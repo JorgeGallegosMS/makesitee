c3931b0c1baee72424470d362a121e7b99fe50e8
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-06-27 18:27:40 -0700

    1.1

--- makesite.go
+++ makesite.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 	"text/template"
@@ -13,9 +14,23 @@ type data struct {
 }
 
 func main() {
-	txtfile := flag.String("file", "first-post.txt", "The name of a text file in the current directory")
+	txtfile := flag.String("file", "", "The name of a text file in the current directory")
+	directory := flag.String("dir", "", "Finds all the text files in the current directory")
 	flag.Parse()
-	renderTemplate("template.tmpl", *txtfile)
+
+	if *txtfile != "" {
+		generateHTMLFile("template.tmpl", *txtfile)
+	}
+
+	if *directory != "" {
+		files := findTxtFiles(*directory)
+
+		for _, file := range files {
+			if strings.Contains(file.Name(), ".txt") {
+				generateHTMLFile("template.tmpl", file.Name())
+			}
+		}
+	}
 }
 
 func readFile(filename string) string {
@@ -27,7 +42,7 @@ func readFile(filename string) string {
 	return string(fileContents)
 }
 
-func renderTemplate(templateFile, txtfile string) {
+func generateHTMLFile(templateFile, txtfile string) {
 	tmplData := data{Content: readFile(txtfile)}
 
 	t := template.Must(template.New(templateFile).ParseFiles(templateFile))
@@ -39,4 +54,14 @@ func renderTemplate(templateFile, txtfile string) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func findTxtFiles(directory string) []os.FileInfo {
+	files, err := ioutil.ReadDir(directory)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return files
 }
